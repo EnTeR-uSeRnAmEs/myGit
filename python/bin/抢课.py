9e4ccd8953f0665c787cb50a1515b80cf37fabe3
unknown <2479880643@qq.com>
2022-03-04 21:05:22 +0800

    抢课

---
+++ python/bin/抢课.py
@@ -0,0 +1,37 @@
+#!usr/bin/python
+#-*- coding: GBK -*-
+from selenium import webdriver
+from time import sleep
+
+account = input('请输入学号：')
+password = input('请输入密码：')
+classes = []
+
+while True:
+    tmp = input('请输入（课程号 教师号）与回车，以0作为输入完毕：').split()
+    if tmp == ['0']:
+        break
+    else:
+        classes.append(tmp)
+
+driver = webdriver.Chrome()
+sleep(0.5)
+driver.get('http://xk.autoisp.shu.edu.cn/')
+sleep(0.5)
+input("请输入回车以开始选课...")
+for i in classes:
+    driver.find_element_by_xpath('//*[@id = "username"]').send_keys(account)
+    driver.find_element_by_xpath('//*[@id = "password"]').send_keys(password)
+    driver.find_element_by_xpath('//button[text() = "登录"]').click()
+    driver.find_element_by_xpath('//*[@name = "rowterm"]').click()
+    driver.find_element_by_xpath('//button[@type = "submit"]').click()
+    driver.find_element_by_xpath('//*[@id = "CourseSelectionStudentFuzzyQuery"]').click()
+    driver.find_element_by_xpath('//*[@name = "CID"]').send_keys(i[0])
+    driver.find_element_by_xpath('//*[@name = "TeachNo"]').send_keys(i[1])
+    driver.find_element_by_xpath('//button[@id="QueryAction"]').click()
+    driver.find_element_by_xpath('//*[@name = "checkclass"]').click()
+    driver.find_element_by_xpath('//button[@id = "CourseCheckAction"]').click()
+    driver.find_element_by_xpath('//span[text() = "关闭"]').click()
+input()
+
+driver.quit()
