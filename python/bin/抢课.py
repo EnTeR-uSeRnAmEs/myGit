9fa647ad69a51b730bbacb957f09090df8e2fd0f
unknown <2479880643@qq.com>
2022-03-05 11:49:27 +0800

    myGit

--- python/bin/抢课.py
+++ python/bin/抢课.py
@@ -2,6 +2,7 @@
 #-*- coding: GBK -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 account = input('请输入学号：')
@@ -15,7 +16,9 @@ while True:
     else:
         classes.append(tmp)
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])#禁止打印日志
+driver = webdriver.Chrome(options=chrome_options)
 sleep(1)
 driver.get('http://xk.autoisp.shu.edu.cn/')
 sleep(1)
@@ -23,17 +26,20 @@ input("请输入回车以开始选课...")
 driver.find_element(By.ID, "username").send_keys(account)
 driver.find_element(By.ID, "password").send_keys(password)
 driver.find_element(By.XPATH, '//button[text() = "登录"]').click()
+driver.find_element(By.NAME, 'rowterm').click()
+driver.find_element(By.XPATH, '//span[text() = "确定"]').click()
+
 for i in classes:
-    driver.find_element(By.XPATH, '//span[text() = "选课"]').click()
-    driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
+    # driver.find_element(By.XPATH, '//span[text() = "选课"]').click()
+    # driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
     driver.find_element(By.XPATH, '//*[@id = "CourseSelectionStudentFuzzyQuery"]').click()
     driver.find_element(By.XPATH, '//*[@name = "CID"]').send_keys(i[0])
     driver.find_element(By.XPATH, '//*[@name = "TeachNo"]').send_keys(i[1])
     driver.find_element(By.XPATH, '//button[@id="QueryAction"]').click()
     driver.find_element(By.XPATH, '//*[@name = "checkclass"]').click()
-    driver.find_element_by_xpath('//button[@id = "CourseCheckAction"]').click()
-    driver.find_element_by_xpath('//span[text() = "关闭"]').click()
+    driver.find_element(By.XPATH, '//button[@id = "CourseCheckAction"]').click()
+    driver.find_element(By.XPATH, '//span[text() = "关闭"]').click()
     sleep(0.5)
-input()
+input("选课结束！（按回车以退出）")
 
 driver.quit()
