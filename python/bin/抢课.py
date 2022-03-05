e08fa65fa21e54a014345342a0642de1debac047
unknown <2479880643@qq.com>
2022-03-05 11:30:37 +0800

    myGit

--- python/bin/抢课.py
+++ python/bin/抢课.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 #-*- coding: GBK -*-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 
 account = input('请输入学号：')
@@ -19,17 +20,17 @@ sleep(1)
 driver.get('http://xk.autoisp.shu.edu.cn/')
 sleep(1)
 input("请输入回车以开始选课...")
-driver.find_element_by_xpath('//*[@id = "username"]').send_keys(account)
-driver.find_element_by_xpath('//*[@id = "password"]').send_keys(password)
-driver.find_element_by_xpath('//button[text() = "登录"]').click()
+driver.find_element(By.ID, "username").send_keys(account)
+driver.find_element(By.ID, "password").send_keys(password)
+driver.find_element(By.XPATH, '//button[text() = "登录"]').click()
 for i in classes:
-    driver.find_element_by_xpath('//span[text() = "选课"]').click()
-    driver.find_element_by_xpath('//button[@type = "submit"]').click()
-    driver.find_element_by_xpath('//*[@id = "CourseSelectionStudentFuzzyQuery"]').click()
-    driver.find_element_by_xpath('//*[@name = "CID"]').send_keys(i[0])
-    driver.find_element_by_xpath('//*[@name = "TeachNo"]').send_keys(i[1])
-    driver.find_element_by_xpath('//button[@id="QueryAction"]').click()
-    driver.find_element_by_xpath('//*[@name = "checkclass"]').click()
+    driver.find_element(By.XPATH, '//span[text() = "选课"]').click()
+    driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
+    driver.find_element(By.XPATH, '//*[@id = "CourseSelectionStudentFuzzyQuery"]').click()
+    driver.find_element(By.XPATH, '//*[@name = "CID"]').send_keys(i[0])
+    driver.find_element(By.XPATH, '//*[@name = "TeachNo"]').send_keys(i[1])
+    driver.find_element(By.XPATH, '//button[@id="QueryAction"]').click()
+    driver.find_element(By.XPATH, '//*[@name = "checkclass"]').click()
     driver.find_element_by_xpath('//button[@id = "CourseCheckAction"]').click()
     driver.find_element_by_xpath('//span[text() = "关闭"]').click()
     sleep(0.5)
