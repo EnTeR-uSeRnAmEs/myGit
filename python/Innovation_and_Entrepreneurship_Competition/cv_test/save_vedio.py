d51a079d29dfec12dbbf365ab348adbfc6c72aa4
EnTeR-uSeRnAmEs <2479880643@qq.com>
2022-03-19 20:33:10 +0800

    myGit

---
+++ python/Innovation_and_Entrepreneurship_Competition/cv_test/save_vedio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#-*- coding: GBK -*-
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("c://Users//24798//Desktop//test_360.mp4")
+
+#fourcc是一种编码格式，我们保存视频时要指定文件名、编码格式、FPS、输出尺寸、颜色模式
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))   #输出文件名、FourCC、帧率、大小
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        #cv2.flip(img,flag)  翻转图像（1水平翻转、0垂直翻转、-1水平垂直翻转）
+        frame = cv2.flip(frame,1)
+
+        # 写入视频帧
+        out.write(frame)
+
+        # cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
--- python/Innovation_and_Entrepreneurship_Competition/cv_test/test.py
+++ python/Innovation_and_Entrepreneurship_Competition/cv_test/test.py
@@ -3,7 +3,7 @@
 
 import cv2
  
-vc = cv2.VideoCapture(r'C://Users//24798//Desktop//test_360.mp4')  # 读入视频文件，命名cv
+vc = cv2.VideoCapture(r'E://电影//remake//1_1080.mp4')  # 读入视频文件，命名cv
 n = 1  # 计数
  
 if vc.isOpened():  # 判断是否正常打开
@@ -19,8 +19,13 @@ while rval:  # 循环读取视频帧
     if (n % timeF == 0):  # 每隔timeF帧进行存储操作
         i += 1
         print(i)
-        cv2.imwrite(r'F://test//360-30//{}.jpg'.format(i), frame)  # 存储为图像
+        # cv2.imwrite(r'F://test//360-30//{}.jpg'.format(i), frame)  # 存储为图像
+        cv2.imshow("frame", frame)
     n = n + 1
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    else:
+        cv2.imwrite(r'C://Users//24798//Desktop//test//{}.jpg'.format(i), frame)
 vc.release()
+cv2.destroyAllWindows()
 # 原文链接：https://blog.csdn.net/qq_36535414/article/details/109143968
