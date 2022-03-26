63e0b28dfad29d5719a513ae4f6964cf7f60e689
EnTeR-uSeRnAmEs <2479880643@qq.com>
2022-03-26 10:26:32 +0800

    2022/03/26 每日一报

---
+++ leetcode/20.有效的括号.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ */
+
+//private include
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+        for (int i = 0; i < int(s.length()) / 2; ++i) {
+            
+        }
+    }
+};
+// @lc code=end
+
